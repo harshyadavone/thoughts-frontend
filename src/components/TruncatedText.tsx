8d8e2ba576c8bf74e293baa1e302787c7391d030
Harsh Dana <harshdana1@gmail.com>
2024-06-12 21:19:31 +0530

    minor changes

--- src/components/TruncatedText.tsx
+++ src/components/TruncatedText.tsx
@@ -9,7 +9,7 @@ interface TruncatedTextProps {
 const TruncatedText: React.FC<TruncatedTextProps> = ({ text, maxLength, dangerous }) => {
   const truncatedText = text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
 
-  return dangerous ? <div dangerouslySetInnerHTML={{__html: truncatedText}}  /> : <span className="font-other tracking-wide">{truncatedText}</span>;
+  return dangerous ? <div className="font-other" dangerouslySetInnerHTML={{__html: truncatedText}}  /> : <span className="font-other tracking-wide">{truncatedText}</span>;
 };
 
 export default TruncatedText;
